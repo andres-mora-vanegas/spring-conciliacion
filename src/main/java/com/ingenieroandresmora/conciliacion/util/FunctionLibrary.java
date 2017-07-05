2ddbf6e0d629073206bc5294f87f23fdf0e8c8c1
AREANDINA\fmora10 <fmora10@areandina.edu.co>
2017-07-05 18:08:10 -0500

    Creación de la función de encriptación

---
+++ src/main/java/com/ingenieroandresmora/conciliacion/util/FunctionLibrary.java
@@ -0,0 +1,35 @@
+package com.ingenieroandresmora.conciliacion.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FunctionLibrary {
+
+	public String encript(String str) {
+
+		String encripted = "";
+		String privateKey = "!#_@.22" + str;
+
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("MD5");
+			md.update(privateKey.getBytes());
+
+			byte byteData[] = md.digest();
+
+			// convert the byte to hex format method 1
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			encripted = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			encripted = str;
+		}
+
+		return encripted;
+
+	}
+}
